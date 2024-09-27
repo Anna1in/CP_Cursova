71515f27defa7a68aff1ce70528af2987cd554cf
Anna <anuta2106200506@gmail.com>
2024-09-27 16:58:11 +0200

    Додана нова папка my_folder

---
+++ main.c
@@ -0,0 +1,4 @@
+h_of_add_multiplication_operation_commit //\ This\ is\ a\ change
+h_of_add_multiplication_operation_commit This\ is\ a\ chenge
+h_of_add_multiplication_operation_commit The\ entire\ Pro\ Git\ book\,\ written\ by\ Scott\ Chacon\ and\ Ben\ Straub\ and\ published\ by\ Apress\,\ is\ available\ here.\ All\ content\ is\ licensed\ under\ the\ Creative\ Commons\ Attribution\ Non\ Commercial\ Share\ Alike\ 3.0\ license.\ Print\ versions\ of\ the\ book\ are\ available\ on\ Amazon.com.
+"// This is a change. The entire Pro Git book, written by Scott Chacon and Ben Straub and published by Apress, is available here. All content is licensed under the Creative Commons Attribution Non Commercial Share Alike 3.0 license. Print versions of the book are available on Amazon.com." 
